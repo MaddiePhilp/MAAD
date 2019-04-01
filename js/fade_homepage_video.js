d51d29c898011d02e493e288dae344efb0c0cb15
Maddie <map48@aber.ac.uk>
2019-04-01 13:31:58 +0100

    hid page content on page load, reveal on click to eneter site

--- js/fade_homepage_video.js
+++ js/fade_homepage_video.js
@@ -1,6 +1,8 @@
 $(document).ready(function ($) {
-  $('#fadeoutbutton').on('click',function(event){
-    $('#divfade').hide(500);
-    $('#Content').pause(500).fadeIn(500);
+  $("#ContentWrapper").hide();
+	$('#fadeoutbutton').on('click',function(event){
+  $('#divfade').hide(500);
+  $('#ContentWrapper').show(1000);
+  $('#ContentWrapper').pause(500).fadeIn(500);
     });
   });
